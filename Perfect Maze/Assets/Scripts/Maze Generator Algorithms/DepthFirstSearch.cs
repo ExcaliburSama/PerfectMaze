a6c7db5ca51b516e4b9f447b56a255cbb1cbabc1
Adrian <adrianvermeij@gmail.com>
2019-01-04 19:47:01 +0100

    Continued Depth-first search algorithm; now includes cells by assigning each grid piece a north, south, east and west wall.

--- Perfect Maze/Assets/Scripts/Maze Generator Algorithms/DepthFirstSearch.cs
+++ Perfect Maze/Assets/Scripts/Maze Generator Algorithms/DepthFirstSearch.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class DepthFirstSearch : MonoBehaviour
 {
     public GameObject wall;
-    public float wallSpace = 1.0f;
     public int xSize = 5;
     public int ySize = 5;
 
+    private float wallSpace = 1.0f;
+    private int eastWestProcess = 0;
+    private int childProcess = 0;
+    private int termCount = 0;
     private Vector3 initialPos;
     private Vector3 myPos;
     private GameObject tempWall;
     private GameObject wallHolder;
+    private GameObject[] allWalls;
+    [SerializeField]
+    private Cell[] cells;
+
+    [System.Serializable]
+    public class Cell
+    {
+        public bool visited;
+        public GameObject north;
+        public GameObject east;
+        public GameObject west;
+        public GameObject south;
+    }
 
     void Start()
     {
         CreateWalls();
     }
+
     void CreateWalls()
     {
         wallHolder = new GameObject();
@@ -46,6 +63,40 @@ public class DepthFirstSearch : MonoBehaviour
                 tempWall = Instantiate(wall, myPos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;
                 tempWall.transform.parent = wallHolder.transform;
             }
+        }
+        CreateCells();
+    }
+
+    void CreateCells ()
+    {
+        int children = wallHolder.transform.childCount;
+        allWalls = new GameObject[children];
+        cells = new Cell[xSize * ySize];
+
+        //Gets all child objects in wallHolder
+        for (int i = 0; i < children; i++)
+        {
+            allWalls[i] = wallHolder.transform.GetChild(i).gameObject;
+        }
+
+        //Assigns walls to cells
+        for (int cellProcess = 0; cellProcess < cells.Length; cellProcess++)
+        {
+            cells[cellProcess] = new Cell();
+            cells[cellProcess].east = allWalls[eastWestProcess];
+            cells[cellProcess].south = allWalls[childProcess+(xSize+1)*ySize];
+            if (termCount == xSize)
+            {
+                eastWestProcess += 2;
+                termCount = 0;
+            }
+            else
+                eastWestProcess++;
+            termCount++;
+            childProcess++;
+
+            cells[cellProcess].west = allWalls[eastWestProcess];
+            cells[cellProcess].north = allWalls[(childProcess + (xSize + 1) * ySize)+xSize-1];
         }
     }
 
