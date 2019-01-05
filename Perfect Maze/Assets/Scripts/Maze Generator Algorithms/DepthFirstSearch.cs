c36c1f9bf096bb0dbc89ff0377185fa8e4880537
Adrian <adrianvermeij@gmail.com>
2019-01-05 01:07:28 +0100

    Continued Depth-first search Algorithm; Now detects if cell has neighbour or not, and assigns number to each cell.

--- Perfect Maze/Assets/Scripts/Maze Generator Algorithms/DepthFirstSearch.cs
+++ Perfect Maze/Assets/Scripts/Maze Generator Algorithms/DepthFirstSearch.cs
@@ -7,19 +7,24 @@ public class DepthFirstSearch : MonoBehaviour
     public GameObject wall;
     public int xSize = 5;
     public int ySize = 5;
+    public int currentCell;
 
+    private int eastWestProcess;
+    private int childProcess;
+    private int termCount;
+    private int neighbourLength;
+    private int neighbourCheck;
+    private int[] neighbours = new int [4];
+    private int totalCells;
     private float wallSpace = 1.0f;
-    private int eastWestProcess = 0;
-    private int childProcess = 0;
-    private int termCount = 0;
     private Vector3 initialPos;
     private Vector3 myPos;
+    [SerializeField]
+    private Cell[] cells;
     private GameObject tempWall;
     private GameObject wallHolder;
     private GameObject[] allWalls;
-    [SerializeField]
-    private Cell[] cells;
-
+    
     [System.Serializable]
     public class Cell
     {
@@ -98,11 +103,65 @@ public class DepthFirstSearch : MonoBehaviour
             cells[cellProcess].west = allWalls[eastWestProcess];
             cells[cellProcess].north = allWalls[(childProcess + (xSize + 1) * ySize)+xSize-1];
         }
+        CreateMaze();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateMaze()
     {
-        
+        GiveMeNeighbour();
+    }
+
+    void GiveMeNeighbour()
+    {
+        totalCells = xSize * ySize;
+        neighbourCheck = ((currentCell + 1) / xSize);
+        neighbourCheck -= 1;
+        neighbourCheck *= xSize;
+        neighbourCheck += xSize;
+
+        //Checks for neighbours on the west of the current cell.
+        if (currentCell +1 < totalCells && (currentCell+1) != neighbourCheck)
+        {
+            if (cells[currentCell+1].visited == false)
+            {
+                neighbours[neighbourLength] = currentCell + 1;
+                neighbourLength++;
+            }
+        }
+
+        //Checks for neighbours on the east of the current cell.
+        if (currentCell + 1 >= 0 && currentCell != neighbourCheck)
+        {
+            if (cells[currentCell - 1].visited == false)
+            {
+                neighbours[neighbourLength] = currentCell - 1;
+                neighbourLength++;
+            }
+        }
+
+        //Checks for neighbours on the north of the current cell.
+        if (currentCell + xSize < totalCells)
+        {
+            if (cells[currentCell + xSize].visited == false)
+            {
+                neighbours[neighbourLength] = currentCell+xSize;
+                neighbourLength++;
+            }
+        }
+
+        //Checks for neighbours on the south of the current cell.
+        if (currentCell - xSize >= 0)
+        {
+            if (cells[currentCell - xSize].visited == false)
+            {
+                neighbours[neighbourLength] = currentCell - xSize;
+                neighbourLength++;
+            }
+        }
+        for (int i = 0; i < neighbourLength; i++)
+        {
+            Debug.Log(neighbours[i]);
+        }
+
     }
 }
