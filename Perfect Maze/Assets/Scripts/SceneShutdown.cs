2e4651698a3d6985c4dbc225a0296e061d437ceb
Adrian <adrianvermeij@gmail.com>
2019-01-10 05:09:28 +0100

    Added method to close application with button, or by pressing escape.

---
+++ Perfect Maze/Assets/Scripts/SceneShutdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Method to shutdown application.
+/// </summary>
+
+public class SceneShutdown : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            ShutDown();
+        }
+    }
+
+    void ShutDown()
+    {
+        Application.Quit();
+    }
+}
