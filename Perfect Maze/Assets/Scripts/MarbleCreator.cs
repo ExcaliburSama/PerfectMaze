79b6cc32387b857bf321ddb520487877ff393a3d
Adrian <adrianvermeij@gmail.com>
2019-01-10 04:56:25 +0100

    Added slider to change speed of marble, made minor changes to code.

--- Perfect Maze/Assets/Scripts/MarbleCreator.cs
+++ Perfect Maze/Assets/Scripts/MarbleCreator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Script used in creating and managing theMarble and its goal counterpart.
@@ -10,29 +11,40 @@ public class MarbleCreator : MonoBehaviour
 {
     public GameObject  marble, currentMarble, goal, currentgoal;
     public DepthFirstSearch mazeGenerator;
+    public Slider slider;
 
-    private int totalMarbles;
+    private bool marbleActive;
     private float disAdjust = 0.5f;
+    private BallMovement marbleSpeed;
 
     private void Start()
     {
         mazeGenerator = GameObject.FindGameObjectWithTag("mazegenerator").GetComponent<DepthFirstSearch>();
     }
 
+    public void ApplySpeed()
+    {
+        if (marbleActive)//Checks if there's a marble active and then applies the value of the slider to the marble.
+        {
+            marbleSpeed.speed = slider.value;
+        }
+    }
+
     public void CreateMarble()
     {
-        if (mazeGenerator.totalCells > 1 && totalMarbles < 1)//uses totalcells to check if a maze has already been created, if not, no marble can be spawned.
+        if (mazeGenerator.totalCells > 1 && !marbleActive)//uses totalcells to check if a maze has already been created, if not, no marble can be spawned.
         {
             Instantiate(marble, new Vector3 (mazeGenerator.initialPos.x, mazeGenerator.initialPos.y, mazeGenerator.initialPos.z - disAdjust), Quaternion.identity); //disAdjust is so that the goal doesnt spawn in a wall.
             Instantiate(goal, new Vector3 (mazeGenerator.myPos.x, mazeGenerator.myPos.y, mazeGenerator.myPos.z - disAdjust), Quaternion.Euler(-90,0,0));//Uses myPos because it's always the position of the last wall built after a maze is built. 
-            totalMarbles++;
-        }
-        
-        else
-        {
-            //takes current marble in the scene as reference, this because the instantiated marble is a clone of a prefab which isn't inherently recognized.
+            
+            //takes currentMarble in the scene as reference, this because the instantiated marble is a clone of a prefab which isn't inherently recognized.
             currentMarble = GameObject.FindGameObjectWithTag("marble");
             currentgoal = GameObject.FindGameObjectWithTag("goal");
+            marbleSpeed = GameObject.FindGameObjectWithTag("marble").GetComponent<BallMovement>();//used to create a reference to the amrble speed so that i can be adjusted through a slider in the UI.
+            marbleActive = true;
+        }      
+        else
+        {                              
             currentMarble.transform.position = new Vector3(mazeGenerator.initialPos.x, mazeGenerator.initialPos.y, mazeGenerator.initialPos.z - disAdjust); //Use this instead of destroy or setactive, easier on the processing.
             currentgoal.transform.position = new Vector3 (mazeGenerator.myPos.x, mazeGenerator.myPos.y, mazeGenerator.myPos.z - disAdjust);
         }
--- Perfect Maze/Assets/Scripts/Resetter.cs
+++ Perfect Maze/Assets/Scripts/Resetter.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Script for the goal that uses DepthFirstSearch and MArbleCreator to reset the scene.
+/// </summary>
+
 public class Resetter : MonoBehaviour
 {
     public DepthFirstSearch mazeGenerator;
     public MarbleCreator marbleCreator;
 
-    // Start is called before the first frame update
     void Start()
     {
         mazeGenerator = GameObject.FindGameObjectWithTag("mazegenerator").GetComponent<DepthFirstSearch>();
         marbleCreator = GameObject.FindGameObjectWithTag("marbleman").GetComponent<MarbleCreator>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//the goal only resets the maze when its made contact with the marble.
     {
         if (other.gameObject.tag == "marble")
         {
